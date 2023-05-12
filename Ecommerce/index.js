719cf397fec9d37fc4d1d73157bdf93b0fcb618f
hauleduc2404 <hauleduc2404@gmail.com>
2023-05-13 01:20:25 +0700

    Chuc nang dang nhap user

---
+++ Ecommerce/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const dbConnect = require('./config/dbConnect');
+const app = express()
+const dotenv= require('dotenv').config()
+const PORT = process.env.PORT || 4000;
+const authRoute = require("./routes/authRoute");
+const bodyParser = require('body-parser');
+const { notFound, errorHandler } = require('./middlewares/errorHandle');
+const cookieParser = require('cookie-parser');
+dbConnect();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(cookieParser());
+
+app.use("/api/user", authRoute);
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(PORT,() => {
+    console.log(`Server is running  at PORT ${PORT}`);
+});
