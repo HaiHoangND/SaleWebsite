86479066644c41f1cd19305f93eeedb119bc0350
Nguyễn Văn Hiển <hiennv@mirabo-global.com>
2023-05-23 22:25:57 +0700

    thêm thống kê số lượng người dùng

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { RiMoneyDollarCircleLine } from "react-icons/ri";
 import { HiShoppingBag } from "react-icons/hi";
 import { TiShoppingCart } from "react-icons/ti";
+import { FiUsers } from "react-icons/fi";
 import { Column } from "@ant-design/plots";
 import axios from "axios";
 
@@ -125,6 +126,28 @@ const Dashboard = () => {
   useEffect(() => {
     fetchOrders();
   }, []);
+
+  const [countUser, setCountUser] = useState(0);
+  const fetchUsers = async () => {
+    try {
+      const token = localStorage.getItem("access_token");
+      const response = await axios.get(
+        "http://localhost:5000/api/user/all-users",
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      const totalUsers = response.data.length;
+      setCountUser(totalUsers);
+    } catch (error) {
+      throw new Error(error);
+    }
+  };
+  useEffect(() => {
+    fetchUsers();
+  }, []);
   return (
     <div>
       <h3 className="mb-4 title">Dashboard</h3>
@@ -136,6 +159,17 @@ const Dashboard = () => {
           <div>
             <p className="desc">Total Sales</p>
             <h4 className="mb-0 sub-title">$1100</h4>
+          </div>
+        </div>
+      </div>
+      <div className="d-flex justify-content-between align-items-center gap-3 mt-3">
+        <div className="d-flex flex-grow-1 bg-white p-3 roudned-3 gap-3">
+          <div>
+            <FiUsers className="fs-1 mb-3" />
+          </div>
+          <div>
+            <p className="desc">Total Users</p>
+            <h4 className="mb-0 sub-title">{countUser}</h4>
           </div>
         </div>
         <div className="d-flex flex-grow-1 bg-white p-3 roudned-3 gap-3">
